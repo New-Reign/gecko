43b66a54ea156fbbc73e966ea3d6788859ab3125
Callum D E Smith <callumdesmith@new-reign.org>
2014-02-09 18:36:06 +0000

    Gecko project start point.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gecko::Application.config.secret_token = '480a4402670118fa96c5f094e37478a06d01b52bd177810310309f44fdf81238d7b0ba8180f1300a9dd789d85fb21fd66bddf143b17ec48db5cb589a0faf56e6'
